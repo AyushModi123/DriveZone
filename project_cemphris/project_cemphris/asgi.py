f957870cd9a9f9477b2d124acfd316ed743a3b27
AyushModi123 <ayushonweb785@gmail.com>
2024-05-27 16:53:46 +0530

    Initial commit

---
+++ project_cemphris/project_cemphris/asgi.py
@@ -0,0 +1,34 @@
+"""
+ASGI config for project_cemphris project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_cemphris.settings')
+
+import django
+django.setup()
+# from base.urls import websocket_urlpatterns
+websocket_urlpatterns = []
+
+# application = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
+    # WebSocket chat handler
+    'websocket': AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+        URLRouter(
+            websocket_urlpatterns
+        )
+    )),
+})
