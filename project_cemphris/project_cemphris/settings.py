63efa5680d3ea18bae833a69a9ccd3a0ae4a06a8
AyushModi123 <ayushonweb785@gmail.com>
2024-07-03 16:03:18 +0530

    add default db url & update path to settings

--- project_cemphris/project_cemphris/settings.py
+++ project_cemphris/project_cemphris/settings.py
@@ -128,7 +128,7 @@ ASGI_APPLICATION = 'project_cemphris.asgi.application'
 
 DATABASES = {
     'default':
-        dj_database_url.config(default=os.environ.get("DATABASE_URL"))
+        dj_database_url.config(default=os.getenv("DATABASE_URL", 'postgres://postgres:qweasd123@pc_db:5432/project_cemphris'))
 }
 
 
