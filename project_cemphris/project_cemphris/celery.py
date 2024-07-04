9f69a14f283e68a7cae9221a681685641611fd9d
AyushModi123 <ayushonweb785@gmail.com>
2024-07-04 14:17:39 +0530

    add celery in init.py

--- project_cemphris/project_cemphris/celery.py
+++ project_cemphris/project_cemphris/celery.py
@@ -7,10 +7,8 @@ from notif_handler.tasks import send_scheduled_emails
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_cemphris.settings')
 
-django.setup()
-
 app = Celery('project_cemphris')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+app.autodiscover_tasks()
