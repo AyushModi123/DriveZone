2912eba07eecf5a06e64d235ea6d7c98d33ed674
AyushModi123 <ayushonweb785@gmail.com>
2024-07-04 13:41:06 +0530

    add django.setup() in celery

--- project_cemphris/project_cemphris/celery.py
+++ project_cemphris/project_cemphris/celery.py
@@ -1,10 +1,13 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
+import django
 from django.conf import settings
 from notif_handler.tasks import send_scheduled_emails
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_cemphris.settings')
+
+django.setup()
 
 app = Celery('project_cemphris')
 
