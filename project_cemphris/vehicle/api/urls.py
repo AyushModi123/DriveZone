93240318cec4e14c33dc428d2e122603baa4dbe9
AyushModi123 <ayushonweb785@gmail.com>
2024-08-03 18:21:35 +0000

    remove stale vehicle urls

--- project_cemphris/vehicle/api/urls.py
+++ project_cemphris/vehicle/api/urls.py
@@ -7,9 +7,7 @@ router = DefaultRouter()
 
 router.register(r'', viewset=views.VehicleViewSet, basename="")
 
-urlpatterns=[
-    path('', views.VehicleViewSet, name='vehicles'),
-    # path('vehicle/', views.get_vehicle, name='get_vehicle'),
+urlpatterns=[        
     path('upload-image/', views.upload_image, name='upload_vehicle_image')
 ]
 
