85ef4af7019e1ee444a660f1d8215dba2ee0a6b5
AyushModi123 <ayushonweb785@gmail.com>
2024-08-03 18:17:55 +0000

    use default router in vehicle url

--- project_cemphris/vehicle/api/urls.py
+++ project_cemphris/vehicle/api/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
 from . import views
 
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+
+router.register(r'', viewset=views.VehicleViewSet, basename="")
+
 urlpatterns=[
     path('', views.VehicleViewSet, name='vehicles'),
     # path('vehicle/', views.get_vehicle, name='get_vehicle'),
     path('upload-image/', views.upload_image, name='upload_vehicle_image')
 ]
+
+urlpatterns+=router.urls
